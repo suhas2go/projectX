3505c1b59208ddaa393cd5d602a908e0e8c92616
suhas2go <suhas2go@gmail.com>
2015-11-07 20:48:32 -0800

    added Person class

--- newapp/newapp/SignUp.xaml.cs
+++ newapp/newapp/SignUp.xaml.cs
@@ -15,11 +15,16 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
+
+
+
+
 namespace newapp
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
+    
     public sealed partial class SignUp : Page
     {
         public SignUp()
@@ -28,14 +33,58 @@ namespace newapp
         }
 
         private void NewUserCreated_Click(object sender, RoutedEventArgs e)
-        {
+        {   
+            Person p1 = new Person
+            {
+                name = NameInput2.Text,
+                pwd =PasswordInput2.Password,
+                g = ,
+                ftbl=,
+                mv=,
+                disc=,
+                csgo=,
+                age= AgeInput2.Text,
+                contactno=ContactInput2.Text
+                 
+
+
+            };
+            await App.MobileService.GetTable<Person>().InsertAsync(p1);
+            var m1 = new MessageDialog("Data Inserted").ShowAsync();
+            nameTxt.Text = "";
+            emailTxt.Text = "";
             MainPage page = new MainPage();
             this.Frame.Navigate(typeof(MainPage));
         }
 
         private void GenderRadMale_Checked(object sender, RoutedEventArgs e)
         {
-            
+
+        }
+
+        private void GenderRadFemale_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FootballTick1_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MovieTick1_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DiscussionTick1_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CounterStrikeTick1_Checked(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
